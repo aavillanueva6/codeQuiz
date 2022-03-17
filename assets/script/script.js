2858d53c33d07dacf75db8d3cad058bb81ddd982
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-03-17 11:05:12 -0700

    cleans up js comments

--- assets/script/script.js
+++ assets/script/script.js
@@ -40,7 +40,7 @@ const Q5 = {
   wrong3: 'array.lastIndexOf',
 };
 const questionList = [Q1, Q2, Q3, Q4, Q5];
-let i = 0;
+let i;
 let finalScore;
 let timeLeft;
 let answerArray = [];
@@ -73,13 +73,20 @@ const clearScoresBtn = document.querySelector('#clearScoresBtn');
 
 // This section defines functions that will be used to execute the quiz.
 
+/**
+ * this function reinitializes the game timer to 50 seconds
+ */
 function initTimer() {
   timeLeft = 50;
 }
 
+/**
+ * this function calls the initTimer function, displays the time on the preGame screen, and initializes the question iterator
+ */
 function initGame() {
   initTimer();
   preGameTimer.textContent = `Time: ${timeLeft}`; // displays the initial time for the game
+  i = 0;
 }
 
 /**
@@ -100,7 +107,7 @@ function startQuizTimer() {
 }
 
 /**
- * when the correct answer is selected, the remaining questions should increment down and the next question should be displayed.
+ * when the correct answer is selected, the screen flashes green.  The resetFlash function is then called after a 250ms delay to reset the flash animation.
  */
 function correctAnswerClicked() {
   body.classList.add('correctAns');
@@ -108,7 +115,7 @@ function correctAnswerClicked() {
 }
 
 /**
- * when a wrong answer is selected, five seconds should be removed from the remaining time, the remaining questions should increment down and the next question should be displayed.
+ * when a wrong answer is selected, ten seconds are removed from the remaining time, and the screen flashes red.  The resetFlash function is then called after a 250ms delay to reset the flash animation.
  */
 function wrongAnswerClicked() {
   console.log('wrong answer');
@@ -118,6 +125,9 @@ function wrongAnswerClicked() {
   setTimeout(resetFlash, 250);
 }
 
+/**
+ * this function removes the animation classes from the body, so that the animation can fire a second time.
+ */
 function resetFlash() {
   body.classList.remove('correctAns');
   body.classList.remove('wrongAns');
@@ -128,13 +138,18 @@ function resetFlash() {
  */
 function nextQuestion() {
   answerArray = []; //reinitialize variable.
+  // This if statement is used to end the game if either the timer has reached 0, or all of the questions have been asked and answered.
+  // if the timeLeft is negative (due to time penalty for a wrong answer), the timeLeft is set to 0, as the minimum score possible.
   if (i == questionList.length || timeLeft <= 0) {
     timeLeft = Math.max(0, timeLeft);
     clearInterval(timerInterval);
     endGame();
     return;
   }
-  question = questionList[i];
+
+  question = questionList[i]; // selects the next question from the questionList array
+
+  // this section stores the questions answers in an array, then loops through each of those to generate a random order to display them.  If the randOrder numer is 0, the answer is added to the beginning of the answerArray, if it is 1, the answer is added to the end of the answerArray.
   let questionAnswers = [
     question.correct,
     question.wrong1,
@@ -157,12 +172,14 @@ function nextQuestion() {
   ans4Btn.textContent = answerArray[3];
 }
 
+/**
+ * This funciton ends the game.  It sets the score to equal the timeLeft and displays the score on the postGame screen
+ */
 function endGame() {
   finalScore = timeLeft; // might need to put some error handling in here, if timeLeft is negative (wrong answer clicked with less than 10 seconds left)
   document.querySelector('#finalScoreDisplay').textContent = finalScore;
 
   // this handles if the user was at the highscore view when the timer runs out
-  // need to add a data-attr for if highscore list is visible. and only execute next few lines if that is true.
   if (highscoreView.dataset.visible === 'true') {
     document.querySelector('#createdHighScoreList').remove(); // removes the score list from the DOM when the highscore screen is left.  This is required to prevent multiple score lists from being displayed.
     highscoreView.classList.remove('visible');
@@ -178,6 +195,9 @@ function endGame() {
   document.querySelector('#initials').value = '';
 }
 
+/**
+ * this function pulls the highscores out of local storage and builds an ol with the scores listed as li's.  The ol is then appended into the highscore div and the page is displayed
+ */
 function displayHighscores() {
   let highScoreArray = JSON.parse(localStorage.getItem('scores'));
   let highScoreList = document.createElement('div');
@@ -209,21 +229,17 @@ function displayHighscores() {
   highscoreView.classList.add('visible');
 }
 
-// button listeners
+// event listeners
+
 // start button begins the game.  It starts the game timer, hides the startGameScreen, and enables the inGameScreen
 startBtn.addEventListener('click', function () {
-  console.log('start button clicked');
   startQuizTimer();
-  console.log('after timer call');
   // classList.remove and .add are used to change which screen is visible.
   startGameView.classList.remove('visible');
   startGameView.classList.add('hidden');
   inGameView.classList.remove('hidden');
   inGameView.classList.add('visible');
   nextQuestion();
-
-  // clearFrontScreen();
-  // runQuiz();
 });
 
 // answer buttons submit the users response to be checked for correct / incorrect, and run the appropriate function based on that result
@@ -271,25 +287,28 @@ ans4Btn.addEventListener('click', function () {
   nextQuestion();
 });
 
-// submit button submits the users initials to the high score list.  It hides the postGameScreen, and enables the highscoreScreen
+// submit button submits the users initials and score to the high score list.  It hides the postGameScreen, and enables the highscoreScreen
 submitBtn.addEventListener('click', function (event) {
   // update high score list
   event.preventDefault(); // this prevents the form default action.
+
+  // this block stores the user input from the initials box as newScoreInitials.  It verifies that the input length is between 1 and 3 (inclusive) characters and sets the input to uppercase.
   let newScoreInitials = document.querySelector('#initials').value;
   if (newScoreInitials.length < 1 || newScoreInitials.length > 3) {
     document.querySelector('#errorMsg').setAttribute('class', 'visible');
     return;
   }
   document.querySelector('#errorMsg').setAttribute('class', 'hidden');
-
   newScoreInitials = newScoreInitials.toUpperCase();
   console.log(newScoreInitials);
 
+  // this creates a newScore object with key/value pairs for initials and score
   let newScore = {
     initials: newScoreInitials,
     score: finalScore,
-  }; // object storing initials and final score on each attempt
+  };
 
+  // this section adds the newScore into the score array, it has functionality built into it to add the new score at the appropriate place in the list to order from highest to lowest score.  If the list exceeds ten items, it removes the last score, to only display the top ten scores.
   // this adds newScore to the scoresArray if it is the first time a score is logged
   console.log(scoresArray);
   if (scoresArray.length === 0) {
@@ -303,10 +322,9 @@ submitBtn.addEventListener('click', function (event) {
         break; // exits the for loop once the newScore is added to the scoresArray.
       }
     }
-    // there has to be a cleaner way to do this
     // the purpose of this section is to add the score to the array if the value is less than the worst score currently in the array.
-    let testNumber = scoresArray[scoresArray.length - 1].score;
-    if (finalScore <= testNumber) {
+    let lowScore = scoresArray[scoresArray.length - 1].score;
+    if (finalScore <= lowScore) {
       scoresArray.push(newScore);
     }
   }
@@ -315,29 +333,24 @@ submitBtn.addEventListener('click', function (event) {
     scoresArray.pop();
   }
 
-  console.log(newScore);
-  console.log(scoresArray);
   localStorage.setItem('scores', JSON.stringify(scoresArray));
-  startGameView.setAttribute('data-previous', 'true');
+  startGameView.setAttribute('data-previous', 'true'); // while the actual previous screen at this point is postGame, it is set to startGame at this point so that the goBack button will function correctly.
   inGameView.setAttribute('data-previous', 'false');
   postGameView.setAttribute('data-previous', 'false');
-  // change to high score screen
-  displayHighscores();
+  displayHighscores(); // change to high score screen
 });
 
-// go back button returns the user from the highscoreScreen to the startGameScreen.  it also reinitializes the timer and the for iterator.
+// go back button returns the user from the highscoreScreen to the previous screen.  It also reinitializes the game (if going back to the startGame view).
 goBackBtn.addEventListener('click', function () {
   document.querySelector('#createdHighScoreList').remove(); // removes the score list from the DOM when the highscore screen is left.  This is required to prevent multiple score lists from being displayed.
   highscoreView.dataset.visible = 'false';
   if (startGameView.dataset.previous === 'true') {
-    initTimer();
-    inGameTimer.textContent = `Time: ${timeLeft}`;
+    initGame();
     // classList.remove and .add are used to change which screen is visible.
     highscoreView.classList.remove('visible');
     highscoreView.classList.add('hidden');
     startGameView.classList.remove('hidden');
     startGameView.classList.add('visible');
-    i = 0;
   } else if (inGameView.dataset.previous === 'true') {
     highscoreView.classList.remove('visible');
     highscoreView.classList.add('hidden');
@@ -360,6 +373,7 @@ clearScoresBtn.addEventListener('click', function () {
   document.querySelector('#createdHighScoreList').classList.add('hidden');
 });
 
+// These next three listeners are looking for a click on the highscore list links at the top of each page.  If they are clicked the highscore list is shown and a state attribute is used to track which screen the user was previously at, in order to navigate back to it later.
 body
   .querySelector('#preGameHighScoreLink')
   .addEventListener('click', function () {
